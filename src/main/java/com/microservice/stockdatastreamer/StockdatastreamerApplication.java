c96a6df3939f9e423769f630a60b051e9a1e439b
ltidden <ltidden@gmail.com>
2024-03-08 14:18:04 +0100

    [ADD] Spring configuration

---
+++ src/main/java/com/microservice/stockdatastreamer/StockdatastreamerApplication.java
@@ -0,0 +1,13 @@
+package com.microservice.stockdatastreamer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockdatastreamerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockdatastreamerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservice/stockdatastreamer/StockdatastreamerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.stockdatastreamer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockdatastreamerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
