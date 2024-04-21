575250a97341c34b37e0ae9ac80b3268da87dc46
ltidden <ltidden@gmail.com>
2024-04-21 17:59:51 +0200

    feat: added date validation

--- src/main/java/com/microservice/stockdatastreamer/core/DataHandler.java
+++ src/main/java/com/microservice/stockdatastreamer/core/DataHandler.java
@@ -1,13 +1,22 @@
 package com.microservice.stockdatastreamer.core;
 
+import com.fasterxml.jackson.core.JsonGenerator;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.microservice.stockdatastreamer.config.KafkaConfig;
 import com.microservice.stockdatastreamer.exception.DataValidationException;
 import com.microservice.stockdatastreamer.producer.StockDataProducer;
 import com.microservice.stockdatastreamer.service.AlphaVantageService;
 import com.microservice.stockdatastreamer.validate.Validator;
+import org.apache.tomcat.util.json.JSONParser;
+import org.joda.time.DateTime;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DataHandler {
 
@@ -17,25 +26,54 @@ public class DataHandler {
         this.restTemplateBuilder = restTemplateBuilder;
     }
 
-    public void fetchAndValidateData() {
+    public void fetchAndValidateData(boolean realFetch) throws DataValidationException {
         AlphaVantageService apiDataConsumer = new AlphaVantageService(restTemplateBuilder);
-        String data = apiDataConsumer.fetchData();
+        String data;
+       if (DateTime.now().getDayOfWeek() <= 5) {
+           data = apiDataConsumer.fetchData(realFetch);
+       } else {
+           data = "No data available on weekends.";
+       }
 
         try {
             Validator validator = new Validator();
-            ProcessingReport APIDataProcessingReport =  validator.validateResponseMetaData(data);
+            if (data.equals("No data available on weekends.")) {
+                System.out.println(data + "Date:" + DateTime.now());
+            }else {
+                ProcessingReport APIDataProcessingReport =  validator.validateResponseMetaData(data);
 
-            System.out.println("APIDataProcessingReport: " + APIDataProcessingReport);
-            if (APIDataProcessingReport.isSuccess()) {
-                System.out.println("Data Validation Successful");
-                KafkaConfig kafkaConfig = new KafkaConfig();
-                StockDataProducer stockDataProducer = new StockDataProducer(kafkaConfig.kafkaTemplate());
-                stockDataProducer.sendValidData(data);
-            } else {
-                throw new DataValidationException("Data Validation Failed");
+//            Logger logger = Logger.getLogger(DataHandler.class.getSimpleName());
+//            logger.log(Level.INFO, "APIDataProcessingReport: " + APIDataProcessingReport);
+
+                System.out.println("APIDataProcessingReport: " + APIDataProcessingReport);
+                if (APIDataProcessingReport.isSuccess()) {
+
+                    int dateindex = data.indexOf("3. Last Refreshed");
+                    if (dateindex != -1) {
+                        String date = data.substring(dateindex + 21, dateindex + 40);
+                        date = date.replace(" ", "T");
+                        DateTime dateTime = new DateTime(date);
+
+                        if (dateTime.isBefore(DateTime.now()) &&
+                                ( DateTime.now().getMinuteOfHour() - dateTime.getMinuteOfHour()) < 5 ) {
+                            KafkaConfig kafkaConfig = new KafkaConfig();
+                            StockDataProducer stockDataProducer = new StockDataProducer(kafkaConfig.kafkaTemplate());
+                            stockDataProducer.sendValidData(data);
+                        } else {
+                            throw new DataValidationException("Refresh date is not valid or not in the last 5 minutes.");
+                        }
+                    } else {
+                        throw new DataValidationException("Refresh date is not found.");
+                    }
+
+                } else {
+//                logger.log(Level.SEVERE, "Data Validation Failed: " + data);
+                    System.err.println("Data Validation Failed: " + data);
+                    throw new DataValidationException("Data Validation Failed");
+                }
             }
         } catch (Exception e) {
-            throw new RuntimeException("Error while validating data", e.getCause());
+            throw new DataValidationException(e.getMessage());
         }
     }
 
--- src/main/java/com/microservice/stockdatastreamer/core/Runner.java
+++ src/main/java/com/microservice/stockdatastreamer/core/Runner.java
@@ -1,6 +1,7 @@
 package com.microservice.stockdatastreamer.core;
 
 
+import com.microservice.stockdatastreamer.exception.DataValidationException;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -8,9 +9,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class Runner {
 
-    @Scheduled(fixedRate = 5000)
-    public static void runDataStreamer() {
+    @Scheduled(fixedRate = 308000) // > 5 minutes
+    public static void runDataStreamer() throws DataValidationException {
         DataHandler dataHandler = new DataHandler(new RestTemplateBuilder());
-        dataHandler.fetchAndValidateData();
+        dataHandler.fetchAndValidateData(true);
     }
 }
